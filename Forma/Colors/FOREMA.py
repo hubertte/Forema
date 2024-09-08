15e54e5398f3107be439eb344e81e00979aaeb47
hubertte <121490817+hubertte@users.noreply.github.com>
2024-09-08 10:46:45 +0200

    Add files via upload

---
+++ Forma/Colors/FOREMA.py
@@ -0,0 +1,47 @@
+### 2 STARS OF POLAND (C)
+### MAKE COLORMA WORK AGAIN!
+
+
+class FORE:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    BRIGHT_BLACK = '\033[90m'
+    BRIGHT_RED = '\033[91m'
+    BRIGHT_GREEN = '\033[92m'
+    BRIGHT_YELLOW = '\033[93m'
+    BRIGHT_BLUE = '\033[94m'
+    BRIGHT_MAGENTA = '\033[95m'
+    BRIGHT_CYAN = '\033[96m'
+    BRIGHT_WHITE = '\033[97m'
+    RESET = '\033[0m'  # Reset color to default
+
+
+
+
+
+class BackgroundColors:
+    BLACK = '\033[40m'
+    RED = '\033[41m'
+    GREEN = '\033[42m'
+    YELLOW = '\033[43m'
+    BLUE = '\033[44m'
+    MAGENTA = '\033[45m'
+    CYAN = '\033[46m'
+    WHITE = '\033[47m'
+    BRIGHT_BLACK = '\033[100m'
+    BRIGHT_RED = '\033[101m'
+    BRIGHT_GREEN = '\033[102m'
+    BRIGHT_YELLOW = '\033[103m'
+    BRIGHT_BLUE = '\033[104m'
+    BRIGHT_MAGENTA = '\033[105m'
+    BRIGHT_CYAN = '\033[106m'
+    BRIGHT_WHITE = '\033[107m'
+    RESET = '\033[0m'  # Reset color to default
+
+
